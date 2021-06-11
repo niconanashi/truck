df39f61cf7b44efbafe7250bd78f88a2857ad587
niconanashi <fgeffge@yahoo.co.jp>
2021-06-12 07:15:31 +0900

    scriptフォルダ追加

---
+++ script/main.js
@@ -0,0 +1,54 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+var defaultParameter_1 = require("./config/defaultParameter");
+var StateManager_1 = require("./StateManager");
+function main(param) {
+    var userSessionParameter = param.sessionParameter;
+    if (userSessionParameter) {
+        assign(defaultParameter_1.sessionParameter, userSessionParameter);
+    }
+    var stateManager = new StateManager_1.StateManager({
+        sessionParameter: defaultParameter_1.sessionParameter,
+        broadcaster: param.broadcasterPlayer
+    });
+    if (!!stateManager.sessionParameter.config.debug &&
+        stateManager.sessionParameter.config.debug.skipLottery) {
+        stateManager.playerList = {};
+        stateManager.playerList[stateManager.broadcaster.id] = new StateManager_1.PlayerInfo({
+            player: stateManager.broadcaster,
+            user: {
+                name: "debug",
+                id: "000000000",
+                isPremium: false
+            },
+            isBroadcaster: true,
+            snakeType: "A"
+        });
+        stateManager.randomGenerator = new g.XorshiftRandomGenerator(2525);
+        stateManager.changeMainGameScene();
+    }
+    else {
+        stateManager.changeTitleScene();
+    }
+}
+exports.main = main;
+function assign(target, _) {
+    var to = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+        if (nextSource != null) {
+            for (var nextKey in nextSource) {
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    if (nextSource[nextKey] != null && typeof nextSource[nextKey] === "object") {
+                        to[nextKey] = assign(to[nextKey], nextSource[nextKey]);
+                    }
+                    else {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+        }
+    }
+    return to;
+}
--- src/utils/entityUtils.ts
+++
@@ -1,7 +0,0 @@
-export function localToGlobal(offset: g.CommonOffset): g.CommonOffset {
-	let point = offset;
-	for (let entity: g.E | g.Scene | undefined = this; entity instanceof g.E; entity = entity.parent) {
-		point = entity.getMatrix().multiplyPoint(point);
-	}
-	return point;
-}
